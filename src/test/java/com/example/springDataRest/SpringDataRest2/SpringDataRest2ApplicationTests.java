e874d170336daea60df9a7008e1db672d7edc0db
John Wolfe <jwolfe890@gmail.com>
2020-09-12 10:35:21 -0500

    first

---
+++ src/test/java/com/example/springDataRest/SpringDataRest2/SpringDataRest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springDataRest.SpringDataRest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
